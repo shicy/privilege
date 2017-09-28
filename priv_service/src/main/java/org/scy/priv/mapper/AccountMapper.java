e8e743d0292b35e437b69d1728b2d198e1f79c67
shicy <shicy85@163.com>
2017-09-28 18:17:05 +0800

    数据库脚本映射文件修改

--- priv_service/src/main/java/org/scy/priv/mapper/AccountMapper.java
+++ priv_service/src/main/java/org/scy/priv/mapper/AccountMapper.java
@@ -1,11 +1,13 @@
 package org.scy.priv.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.scy.priv.model.AccountModel;
 
 /**
  * 帐户信息相关映射类
  * Created by shicy on 2017/9/27.
  */
+@Mapper
 @SuppressWarnings("unused")
 public interface AccountMapper {
 
