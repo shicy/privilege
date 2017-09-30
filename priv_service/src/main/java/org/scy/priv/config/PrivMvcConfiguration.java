b02db2ae9f16f040014bae19d3d006068b4fb1bd
shicy <shicy85@163.com>
2017-09-30 16:36:38 +0800

    添加配置类

--- priv_service/src/main/java/org/scy/priv/config/PrivMvcConfiguration.java
+++ priv_service/src/main/java/org/scy/priv/config/PrivMvcConfiguration.java
@@ -1,7 +1,5 @@
 package org.scy.priv.config;
 
-import org.scy.priv.interceptor.AccessTokenInterceptor;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -9,16 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * 配置
  * Created by shicy on 2017/9/27.
  */
-@Configuration
+//@Configuration
 @SuppressWarnings("unused")
 public class PrivMvcConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         super.addInterceptors(registry);
-
-        // AccessToken 拦截器
-        registry.addInterceptor(new AccessTokenInterceptor()).addPathPatterns("/**");
     }
 
 }
