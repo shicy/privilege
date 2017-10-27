77cb3f192c888e8a69b8e58429c1444e0010064b
shicy <shicy85@163.com>
2017-10-27 16:41:41 +0800

    用户相关服务类

--- priv_service/src/main/java/org/scy/priv/service/UserService.java
+++ priv_service/src/main/java/org/scy/priv/service/UserService.java
@@ -1,18 +1,29 @@
 package org.scy.priv.service;
 
-import org.scy.priv.model.User;
+import org.scy.priv.model.UserModel;
+
+import java.util.List;
 
 /**
  * 用户相关服务类
  * Created by shicy on 2017/9/3
  */
+@SuppressWarnings("unused")
 public interface UserService {
 
     /**
-     * 根据用户名称获取用户信息
-     * @param name
-     * @return
+     * 根据用户编号获取用户信息
      */
-    User findUserByName(String name);
+    UserModel getById(int id);
+
+    /**
+     * 根据用户名称获取用户信息
+     */
+    UserModel getByName(String name);
+
+    /**
+     * 根据用户编号集获取用户信息
+     */
+    List<UserModel> getByIds(int[] ids);
 
 }
--- priv_service/src/main/java/org/scy/priv/service/impl/UserServiceImpl.java
+++ priv_service/src/main/java/org/scy/priv/service/impl/UserServiceImpl.java
@@ -2,8 +2,11 @@ package org.scy.priv.service.impl;
 
 import org.scy.common.web.service.MybatisBaseService;
 import org.scy.priv.model.User;
+import org.scy.priv.model.UserModel;
 import org.scy.priv.service.UserService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 用户相关服务实现类
@@ -13,7 +16,17 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends MybatisBaseService implements UserService {
 
     @Override
-    public User findUserByName(String name) {
+    public UserModel getById(int id) {
+        return null;
+    }
+
+    @Override
+    public UserModel getByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<UserModel> getByIds(int[] ids) {
         return null;
     }
 
