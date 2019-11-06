698d0c3af681906fb1718588988197fe555f853e
shicy <shicy85@163.com>
2019-11-06 18:02:51 +0800

    Privilege客户端配置

--- priv_api/src/main/java/org/scy/priv/PrivilegeClientConfiguration.java
+++ priv_api/src/main/java/org/scy/priv/PrivilegeClientConfiguration.java
@@ -2,12 +2,14 @@ package org.scy.priv;
 
 import feign.RequestInterceptor;
 import org.scy.common.configs.BaseFeignConfiguration;
+import org.springframework.context.annotation.Bean;
 
 /**
  * PrivilegeClient拦截器
  */
 public class PrivilegeClientConfiguration extends BaseFeignConfiguration {
 
+    @Bean
     public RequestInterceptor requestInterceptor() {
         return getAccessTokenInterceptor(null);
     }
